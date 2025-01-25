76764c696689c0cc54c59ffa6d99d67f05afd963
RickContreras <ricardo.contreras568@hotmail.com>
2025-01-25 03:09:17 -0500

    Se trabajo en un escalamiento mas animado

--- Assets/Scripts/Player/Crecimiento.cs
+++ Assets/Scripts/Player/Crecimiento.cs
@@ -1,16 +1,23 @@
+using System.Collections;
 using UnityEngine;
-public class Crecimiento : MonoBehaviour 
+using UnityEngine.UIElements;
+public class Crecimiento : MonoBehaviour
 {
-    public float scaleIncreaseAmount = 0.2f; // Cantidad de aumento de la escala
-    public float scaleDecreaseAmount = 0.2f; // Cantidad de disminución de la escala
+    [Header("Limites de escalamiento")]
     public float maxScale = 1.5f; // Tamaño máximo del círculo
+    public float minScale = 0.5f; // Tamaño mínimo del círculo
+    [Header("Configuracion del escalamiento")]
+    public float scaleAmount = 0.2f; // Cantidad para la escala
+    public AnimationCurve scaleCurve; // Curva de escala
+    public float timeScaling = 0.5f; // Tiempo de escalamiento
     private void OnTriggerEnter2D(Collider2D collision)
     {
         // Aumento de escala
         if (collision.gameObject.CompareTag("Triangle"))
         {
-            // Agranda el círculo aumentando su escala (radio)
-            Vector3 newScale = transform.localScale + new Vector3(scaleIncreaseAmount, scaleIncreaseAmount, 0);
+            Vector3 escala = new Vector3(scaleAmount, scaleAmount, 0);
+            Vector3 newScale = transform.localScale + escala;
+            StartCoroutine(TransicionDeEscalamiento(escala));
             if (newScale.x <= maxScale && newScale.y <= maxScale)
             {
                 transform.localScale = newScale;
@@ -24,12 +31,27 @@ public class Crecimiento : MonoBehaviour
         //Disminución de escala
         if (collision.gameObject.CompareTag("Square"))
         {
-            transform.localScale -= new Vector3(scaleDecreaseAmount, scaleDecreaseAmount, 0);
+            Vector3 escala = new Vector3(scaleAmount, scaleAmount, 0);
+            Vector3 newScale = transform.localScale + escala;
+            StartCoroutine(TransicionDeEscalamiento(escala * (-1)));
+            transform.localScale -= escala;
             if (transform.localScale.x < 0.1f || transform.localScale.y < 0.1f)
             {
-                transform.localScale = new Vector3(0.5f, 0.5f, 0);
+                transform.localScale = new Vector3(minScale, minScale, 0);
             }
             Destroy(collision.gameObject);
         }
     }
+    IEnumerator TransicionDeEscalamiento(Vector3 escala)
+    {
+        Vector3 newScale = transform.localScale + escala;
+        Vector3 oldScale = transform.localScale;
+        float deltaT = timeScaling/10; // 10 pausas
+
+        for (int i = 0; i <= 10; i++) //El maximo de i es 10
+        {
+            transform.localScale = Vector3.LerpUnclamped(oldScale, newScale, scaleCurve.Evaluate(i / 10f)); // 10f se refiere al maximo de i
+            yield return new WaitForSeconds(deltaT);
+        }
+    }   
 }
